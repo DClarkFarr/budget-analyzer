d35fa68cf00dff92dd06c21d98b28cd995dc8db6
Daniel Farr <daniel@aptivada.com>
2024-02-28 21:41:44 -0700

    date raging

--- src/components/Statement/StatementTransactionRange.tsx
+++ src/components/Statement/StatementTransactionRange.tsx
@@ -1,49 +1,80 @@
 import { Transaction } from "@/types/Account/Transaction";
 import { DateTime } from "luxon";
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
+import TransactionsByMonth from "./TransactionsByMonth";
 
 export default function StatementTransactionRange({
-  transactions,
+    transactions,
 }: {
-  transactions: Transaction[];
+    transactions: Transaction[];
 }) {
-  const transactionsByMonth = useMemo(() => {
-    return Object.entries(
-      transactions.reduce((acc, transaction) => {
-        const monthYear = DateTime.fromISO(transaction.date).toFormat("MMM yy");
-        if (!acc[monthYear]) {
-          acc[monthYear] = {
-            count: 0,
-            label: monthYear,
-            timestamp: DateTime.fromISO(transaction.date).toMillis(),
-          };
-        }
-        acc[monthYear].count++;
+    const transactionsByMonth = useMemo(() => {
+        return Object.entries(
+            transactions.reduce((acc, transaction) => {
+                const monthYear = DateTime.fromISO(transaction.date).toFormat(
+                    "MMM yy"
+                );
+                if (!acc[monthYear]) {
+                    acc[monthYear] = {
+                        count: 0,
+                        label: monthYear,
+                        timestamp: DateTime.fromISO(
+                            transaction.date
+                        ).toMillis(),
+                    };
+                }
+                acc[monthYear].count++;
 
-        return acc;
-      }, {} as Record<string, { count: number; label: string; timestamp: number }>)
-    )
-      .map(([, val]) => val)
-      .sort((a, b) => a.timestamp - b.timestamp);
-  }, [transactions]);
-  return (
-    <div>
-      <h3 className="text-lg">Transactions by Month</h3>
-      <div className="months flex flex-wrap gap-2">
-        {transactionsByMonth.map((month) => (
-          <div
-            key={month.timestamp}
-            className="month px-2 py-1 bg-gray-100 rounded text-center flex gap-x-1"
-          >
-            <div className="month__label">{month.label}</div>
-            <div>
-              <div className="month__count font-bold p-1 text-xs leading-none bg-sky-700 text-white rounded-full">
-                {month.count}
-              </div>
+                return acc;
+            }, {} as Record<string, { count: number; label: string; timestamp: number }>)
+        )
+            .map(([, val]) => val)
+            .sort((a, b) => a.timestamp - b.timestamp);
+    }, [transactions]);
+
+    const [dates, setDates] = useState<{ start: string; end: string }>({
+        start: "",
+        end: "",
+    });
+
+    useEffect(() => {
+        const firstDate = Math.min(
+            ...transactions.map((t) => DateTime.fromISO(t.date).toMillis())
+        );
+        const lastDate = Math.max(
+            ...transactions.map((t) => DateTime.fromISO(t.date).toMillis())
+        );
+
+        setDates({
+            start: DateTime.fromMillis(firstDate).toFormat("yyyy-MM-dd"),
+            end: DateTime.fromMillis(lastDate).toFormat("yyyy-MM-dd"),
+        });
+    }, [transactions]);
+
+    return (
+        <div>
+            <h3 className="text-lg">Transactions by Month</h3>
+            <div className="months flex flex-wrap gap-2 mb-8">
+                {transactionsByMonth.map((month) => (
+                    <div
+                        key={month.timestamp}
+                        className="month px-2 py-1 bg-gray-100 rounded text-center flex gap-x-1"
+                    >
+                        <div className="month__label">{month.label}</div>
+                        <div>
+                            <div className="month__count font-bold p-1 text-xs leading-none bg-sky-700 text-white rounded-full">
+                                {month.count}
+                            </div>
+                        </div>
+                    </div>
+                ))}
             </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+
+            <TransactionsByMonth
+                startDate={dates.start}
+                endDate={dates.end}
+                transactions={transactions}
+            />
+        </div>
+    );
 }
---
+++ src/components/Statement/TransactionsByMonth.tsx
@@ -0,0 +1,113 @@
+import { formatCurrency } from "@/methods/currency";
+import { Transaction } from "@/types/Account/Transaction";
+import { DateTime } from "luxon";
+import { useMemo } from "react";
+
+export default function TransactionsByMonth({
+    startDate,
+    endDate,
+    transactions,
+}: {
+    startDate: string;
+    endDate: string;
+    transactions: Transaction[];
+}) {
+    const monthsToShow = useMemo(() => {
+        const months: DateTime[] = [];
+
+        let sd = startDate
+            ? DateTime.fromISO(startDate).startOf("month")
+            : DateTime.now().startOf("year");
+        const ed = endDate
+            ? DateTime.fromISO(endDate)
+            : DateTime.now().startOf("month");
+
+        let c = 0;
+        while (sd < ed) {
+            months.push(sd);
+            sd = sd.plus({ months: 1 });
+            c++;
+
+            if (c > 100) {
+                console.log("you are stupid");
+                break;
+            }
+        }
+
+        return months;
+    }, [startDate, endDate]);
+
+    const transactionsByMonth = useMemo(() => {
+        const map: Record<
+            string,
+            { incoming: number; outgoing: number; net: number }
+        > = {};
+
+        transactions.forEach((t) => {
+            const month = DateTime.fromISO(t.date).toFormat("yyyy-MM");
+            if (!map[month]) {
+                map[month] = { incoming: 0, outgoing: 0, net: 0 };
+            }
+
+            if (t.expenseType === "incoming") {
+                map[month].incoming += t.amount;
+            } else {
+                map[month].outgoing += t.amount;
+            }
+
+            map[month].net +=
+                t.expenseType === "incoming" ? t.amount : -t.amount;
+        });
+
+        return map;
+    }, [transactions]);
+
+    return (
+        <div className="transactions-by-month flex flex-wrap gap-4">
+            {monthsToShow.map((m) => {
+                const formattedMonth = m.toFormat("yyyy-MM");
+
+                if (!transactionsByMonth[formattedMonth]) {
+                    return (
+                        <div key={formattedMonth}>
+                            Not found: {formattedMonth}
+                        </div>
+                    );
+                }
+                return (
+                    <div
+                        className="card border border-rounded"
+                        key={formattedMonth}
+                    >
+                        <div className="card__header px-4 py-2 border-b border-gray-400 text-center font-bold">
+                            {m.toFormat("LLL yyyy")}
+                        </div>
+                        <div className="card__body p-4">
+                            <div className="text-emerald-600">
+                                {formatCurrency(
+                                    transactionsByMonth[formattedMonth].incoming
+                                )}
+                            </div>
+                            <div className="text-red-600">
+                                {formatCurrency(
+                                    transactionsByMonth[formattedMonth].outgoing
+                                )}
+                            </div>
+                            <div
+                                className={`pt-4 font-semibold ${
+                                    transactionsByMonth[formattedMonth].net > 0
+                                        ? "text-emerald-600"
+                                        : "text-red-600"
+                                }`}
+                            >
+                                {formatCurrency(
+                                    transactionsByMonth[formattedMonth].net
+                                )}
+                            </div>
+                        </div>
+                    </div>
+                );
+            })}
+        </div>
+    );
+}

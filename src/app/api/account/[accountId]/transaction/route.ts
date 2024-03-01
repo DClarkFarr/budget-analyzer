d7cf9d39e5c25bacfc304871e821a820f8d46a3f
Daniel Farr <daniel@aptivada.com>
2024-03-01 11:21:37 -0700

    transactions by year

--- src/app/api/account/[accountId]/transaction/route.ts
+++ src/app/api/account/[accountId]/transaction/route.ts
@@ -5,6 +5,7 @@ import {
 } from "@/server/middleware/sessionMiddleware";
 import { getUserAccount } from "@/server/prisma/account.methods";
 import { getAccountTransactions } from "@/server/prisma/account/statement.methods";
+import { DateTime } from "luxon";
 import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
@@ -15,6 +16,8 @@ export const GET = chainMiddleware(
         try {
             const user = req.session.user;
             const accountId = parseInt(params.accountId);
+            const yearString = req.nextUrl.searchParams.get("year");
+            const year = yearString ? parseInt(yearString) : undefined;
             const account = await getUserAccount(user.id, accountId);
 
             if (!account) {
@@ -27,8 +30,15 @@ export const GET = chainMiddleware(
             const withCategories =
                 req.nextUrl.searchParams.get("withCategories") === "true";
 
+            const minDate = year
+                ? DateTime.fromObject({ year, month: 1, day: 1 })
+                : undefined;
+            const maxDate = minDate ? minDate.endOf("year") : undefined;
+
             const transactions = await getAccountTransactions(accountId, {
                 withCategories,
+                minDate: minDate?.toISO() || undefined,
+                maxDate: maxDate?.toISO() || undefined,
             });
 
             return NextResponse.json(transactions);
